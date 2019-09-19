d3b6e487d97f6bab1548090fa78d4c60abaebc64
Paulo Borges <pborges475@gmail.com>
2019-09-19 07:12:20 -0700

    update "main" demo script

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/pborges/iot/pubsub"
 )
@@ -12,6 +13,7 @@ func main() {
 	c2, _ := broker.CreateClient("client2")
 	c3, _ := broker.CreateClient("client3")
 	c4, _ := broker.CreateClient("client4")
+	c5, _ := broker.CreateClient("client5")
 
 	c2.Subscribe(">", func(name string, value pubsub.Datum, ctx pubsub.Context) error {
 		return nil
@@ -32,13 +34,21 @@ func main() {
 	attr, _, _ := c1.CreateAttribute("temp", pubsub.IntegerDefinition{Default: 3}, nil)
 	attr.Update(55)
 
-	c4.CreateAttribute("bob", pubsub.IntegerDefinition{}, func(i interface{}) error {
-		fmt.Println("[AcceptFN          ] ATTR: bob VALUE:", i)
+	c4.CreateAttribute("bob", pubsub.IntegerDefinition{}, func(s pubsub.Source, i interface{}) error {
+		if i.(int64) == 3 {
+			return errors.New("threes are evil")
+		}
 		return nil
 	})
 
 	c4.Publish("client1.temp", 67)
 	c2.Publish("client4.bob", 666)
+
+	c5.Subscribe("client4.*", nil)
+
+	for i := 0; i < 5; i++ {
+		c2.Publish("client4.bob", i)
+	}
 
 	fmt.Println("done")
 }
